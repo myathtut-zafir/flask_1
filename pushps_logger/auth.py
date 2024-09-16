986f6960a561b3b5c2b1dcb545013d143cd697bc
myathtut-zafir <aa.htut@gmail.com>
2024-09-16 20:54:11 +0700

    feat:setup form

--- pushps_logger/auth.py
+++ pushps_logger/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,url_for
+from flask import Blueprint,render_template,url_for,request,redirect
 
 auth=Blueprint("auth",__name__)
 
@@ -6,9 +6,25 @@ auth=Blueprint("auth",__name__)
 def signup():
     return  render_template('signup.html')
 
+@auth.route("/signup",methods=['POST'])
+def signup_post():
+    email=request.form.get('email')
+    name=request.form.get('name')
+    password=request.form.get('password')
+    
+    print(email,name,password)
+    return redirect(url_for('auth.login'))
+
 @auth.route("/login")
 def login():
     return  render_template('login.html')
+
+@auth.route("/login",methods=['POST'])
+def login_post():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    
+    return redirect(url_for('main.profile'))
 
 def logout():
     return "Logout"
