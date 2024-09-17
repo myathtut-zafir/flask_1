4ea490af8e1d2427fd300bf375c756f7d0414b22
myathtut-zafir <aa.htut@gmail.com>
2024-09-17 22:12:13 +0700

    feat: setup create and list

--- pushps_logger/main.py
+++ pushps_logger/main.py
@@ -1,5 +1,8 @@
-from flask import Blueprint,render_template,url_for
+from flask import Blueprint, redirect,render_template,url_for,request,flash,abort
 from flask_login import current_user,login_required
+from .models import Workout
+from .models import User
+from . import db
 
 main=Blueprint("main",__name__)
 
@@ -11,3 +14,28 @@ def index():
 @login_required
 def profile():
     return  render_template('profile.html',name=current_user.name)
+
+@main.route("/new")
+@login_required
+def new_workout():
+    return  render_template('create_workout.html')
+
+@main.route("/new",methods=["POST"])
+@login_required
+def new_workout_post():
+    pushups=request.form.get('pushups')
+    comment=request.form.get('comment')
+
+    workout=Workout(pushups=pushups,comment=comment,author=current_user)
+    db.session.add(workout)
+    db.session.commit()
+
+    flash("success added")
+    return  redirect(url_for('main.user_workouts'))
+
+@main.route('/all')
+@login_required
+def user_workouts():
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+    workouts = user.workouts
+    return render_template('all_workouts.html', workouts=workouts, user=user)
